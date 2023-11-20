05bc6d4044fcd800a4fe6c60319659d338941337
Tingz-sky <tianzezhang0730@163.com>
2023-11-20 17:40:37 -0500

    Modify the generation function

--- generate.py
+++ generate.py
@@ -16,18 +16,25 @@ import pandas as pd
 from sklearn.model_selection import train_test_split, KFold
 from tqdm import tqdm
 
-model_path = "models/ft1"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+data_file_path = "raw_data/backstory.pkl"
+df = pd.read_pickle(data_file_path)
+titles = df["Title"].tolist()
+titles_train, titles_test = train_test_split(titles, test_size=0.1, random_state=42)
+titles_test = titles_test[:50]
+
+model_path = "models/ft1"
 print("using device " + str(device))
-num_output = 100  # number of output desired
+num_output = 50  # number of output desired
 
-# tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
-# model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True).to(device)
-tokenizer = AutoTokenizer.from_pretrained('gpt2-large')
-model = AutoModelForCausalLM.from_pretrained('gpt2-large').to(device)
+tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
+model = AutoModelForCausalLM.from_pretrained(model_path, local_files_only=True).to(device)
+# tokenizer = AutoTokenizer.from_pretrained('gpt2-large')
+# model = AutoModelForCausalLM.from_pretrained('gpt2-large').to(device)
 
-prompt = "This is the story of [PAWN_nameDef], a"  # housemate: "
-input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+for title_test in titles_test:
+    prompt = "This is the story of [PAWN_nameDef], a" + title_test + ":"
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
 
 print(input_ids.get_device())
 
@@ -37,7 +44,7 @@ list_out = []
 sentence_out = []
 # ['Today I believe we can finally get rid of discrimination," said Rep. Mark Pocan (D-Wis.).\n\n"Just look at the']
 for i in range(num_output):
-    outputs = model.generate(input_ids, do_sample=True, max_length=70, temperature=0.8 + 0.03 * i, top_p=1,
+    outputs = model.generate(input_ids, do_sample=True, max_length=70, temperature=1, top_p=1,
                              repetition_penalty=1)
     # TODO output length
     list_out.append(outputs)
@@ -46,4 +53,4 @@ for i in range(num_output):
     generate_sentence = tokenizer.batch_decode(list_out[i], skip_special_tokens=True)
     sentence_out.append(generate_sentence)
 df = pd.DataFrame(sentence_out)
-df.to_pickle('models/gpt2-large.pkl')
+df.to_pickle('models/Story_generate/ft1_50_70.pkl')
